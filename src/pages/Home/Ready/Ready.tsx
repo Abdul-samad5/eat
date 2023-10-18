4941531f0df9c99b4b9ba9d8d39dddcd6668f60a
Abdulazeez Abdulsamad <abdulazeezabdulsamad16@gmail.com>
2023-10-18 21:18:30 +0100

    agains

--- src/pages/Home/Ready/Ready.tsx
+++ src/pages/Home/Ready/Ready.tsx
@@ -7,7 +7,7 @@ export default function Ready() {
             <div className="ml-20 mr-20 mt-24 mb-20">
                  <fieldset className="border-2 text-center border-green">
                     <legend className='text-green text-5xl font-extrabold pr-10 pl-10'>Ready for the best food deals?</legend>
-                    <p className="pr-80 mt-6 pl-80 mb-24">Get quality food for the best prices. Lorem ipsum dolor sit amet consectetur. Lacus sagittis elementum nisl euismod cras nec scelerisque nulla. </p>
+                    <p className="pr-80 mt-6 pl-80 mb-24">{"Get quality food for the best prices. Lorem ipsum dolor sit amet consectetur. Lacus sagittis elementum nisl euismod cras nec scelerisque nulla. "}</p>
                        <div className='flex justify-around flex-grow-0'>
                         <div className="mb-24">
                             <Image 
@@ -18,7 +18,7 @@ export default function Ready() {
                             height={70}
                             />
                             <h1 className='text-green uppercase text-3xl font-bold pl-20'>Discover</h1>
-                            <p className='pl-16 w-72 pt-4'>Enjoy Browsing through a variety of delicious chef-created food deals From local favorites to exotic cuisines. , there's something for everyone.</p>
+                            <p className='pl-16 w-72 pt-4'>{"Enjoy Browsing through a variety of delicious chef-created food deals From local favorites to exotic cuisines. , there's something for everyone."}</p>
                         </div>
                         <div>
                           <Image 
@@ -29,7 +29,7 @@ export default function Ready() {
                             height={70}
                             />
                             <h1 className='text-green uppercase text-3xl font-bold pl-20'>Join deal</h1>
-                            <p className='pt-4 w-72 pl-10'>Top up your wallet and join food deals you love. You can also buy immediately if you can’t wait. Your money will not be deducted until the deal is successful.</p>
+                            <p className='pt-4 w-72 pl-10'>{"Top up your wallet and join food deals you love. You can also buy immediately if you can’t wait. Your money will not be deducted until the deal is successful."}</p>
                         </div>
                         <div>
                         <Image 
@@ -40,7 +40,7 @@ export default function Ready() {
                             height={70}
                             />
                             <h1 className='text-green uppercase text-3xl font-bold pl-18'>Deliver</h1>
-                            <p className='pl-8 pr-5 w-72 pt-4'>If the deal is successful Sit back and relax while the chef prepares your order. Your delicious meal will be delivered right to your doorstep.</p>
+                            <p className='pl-8 pr-5 w-72 pt-4'>{"If the deal is successful Sit back and relax while the chef prepares your order. Your delicious meal will be delivered right to your doorstep."}</p>
                         </div>
                        </div>
                  </fieldset>
