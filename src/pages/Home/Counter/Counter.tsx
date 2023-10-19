afe0032b87f473e42b325fd490a5beb49aa48aaf
Abdulazeez Abdulsamad <abdulazeezabdulsamad16@gmail.com>
2023-10-19 05:52:34 +0100

    today d

--- src/pages/Home/Counter/Counter.tsx
+++ src/pages/Home/Counter/Counter.tsx
@@ -28,12 +28,11 @@ export default function Counter() {
     
       <div className='flex justify-around items-center text-white '>
         {
-            datas.map((data)=>
-            <div className='pl-10' key={data.id}>
+            datas.map((data, index)=>
+            <div className='pl-10' key={`${data.id}+${index}`}>
                  <h1 className='text-5xl'> {data.count}</h1>
                 <p className="pr-10 text-xl font-normal">{data.text}</p>
             </div>
-           
                 )
         }
             <Image
